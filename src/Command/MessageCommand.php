b40f9980a14b24912a58edeab5189c3496e68616
vartrue <1175398345@qq.com>
2025-07-06 14:22:12 +0800

    feat:命令行

--- src/Command/MessageCommand.php
+++ src/Command/MessageCommand.php
@@ -18,10 +18,10 @@ class MessageCommand extends AbstractCommand
     protected ExcelInterface $excel;
     protected ProgressInterface $progress;
 
-    public function __construct(ContainerInterface $container, ExcelInterface $excel, ProgressInterface $progress)
+    public function __construct(ContainerInterface $container, ExcelInterface $excel)
     {
         $this->container = $container;
-        $this->progress = $progress;
+        $this->excel = $excel;
         parent::__construct('excel:message');
     }
 
@@ -29,37 +29,34 @@ class MessageCommand extends AbstractCommand
     {
         $token = $this->input->getArgument('token');
         $num = $this->input->getOption('num');
+        $progress = $this->input->getOption('progress');
 
-        $this->line("开始获取信息:");
+        $this->info("开始获取信息:");
         do {
-            $progressRecord = $this->progress->getRecordByToken($token);
+            $progressRecord = $this->excel->getProgressRecord($token);
             if (!$progressRecord) {
                 $this->error('未找到进度记录');
                 return;
             }
-            $messages = $this->progress->popMessage($token, $num);
+            $isEnd = false;
+            $messages = $this->excel->popMessageAndIsEnd($token, $num, $isEnd);
             foreach ($messages as $message) {
                 $this->line($message);
             }
-            $isEnd = in_array($progressRecord->progress->status, [
-                    ProgressData::PROGRESS_STATUS_COMPLETE,
-                    ProgressData::PROGRESS_STATUS_FAIL,
-                ]) && empty($messages);
-
             msleep(500);
         } while (!$isEnd);
 
-        if ($progressRecord) {
+        if ($progress) {
             $this->showProgress($token);
         }
     }
-
 
     protected function configure()
     {
         $this->setDescription('View progress messages');
         $this->addArgument('token', InputArgument::REQUIRED, 'The token of excel.');
         $this->addOption('num', 'c', InputOption::VALUE_REQUIRED, 'The message num of excel.', 50);
+        $this->addOption('progress', 'g', InputOption::VALUE_NEGATABLE, 'The progress of export.', true);
 
         $this->addUsage('excel:message 168d8baf7fbc435c8ef18239e932b101');
         $this->addUsage('excel:message 168d8baf7fbc435c8ef18239e932b101 --no-progress');
